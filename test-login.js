a17efe9ec945be5f5a561ed53b886d4c0fdcaf75
Jose Antonio Rodríguez <joseantonio.rodriguezfernandez@telefonica.com>
2015-05-16 17:14:49 +0200

    0-init

---
+++ test-login.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
+var expect = chai.expect;
+
+var appUrl = 'http://mrgamer.github.io/angular-login-example';
+//var appUrl = 'http://localhost:8080';
+
+feature('Login', function() {
+
+  scenario('Correct log in', function() {
+    given('I am logged out', function() {
+
+    });
+    when('I enter in the login form the credentials of a user with role user', function() {
+
+    });
+    and('I click on the "Login" button', function() {
+
+    });
+    then('The app greets the user using her real name', function() {
+
+    });
+    and('the "Logout" button is shown', function() {
+
+    });
+  });
+
+});
